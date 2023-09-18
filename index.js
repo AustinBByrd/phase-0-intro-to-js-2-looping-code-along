71d7e8d44db86b1ef4980e36ccc43bf100e2f72c
Austin Byrd <byrda05@gmail.com>
2023-09-18 11:54:09 -0500

    finishLab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+
+    for (let i = 0; i < namesArray.length; i++) {
+        const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+
+        thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+}
+
+const names = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+const messages = writeCards(names, event);
+console.log(messages);
+
+function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+        console.log(startingNumber);
+        startingNumber--;
+    }
+}
